b0f96efdf58d8e21eb35b6f8a447b768031a55bc
wwnorden <claas-w@gmx.de>
2021-05-02 18:12:48 +0200

    page connection

--- src/AppointmentList.php
+++ src/AppointmentList.php
@@ -2,15 +2,13 @@
 
 namespace WWN\Appointments;
 
+use SilverStripe\CMS\Model\SiteTree;
 use SilverStripe\ORM\DataObject;
-use SilverStripe\ORM\ManyManyList;
 
 /**
  * AppointmentList
  *
  * @package wwn-appointments
- * @property string $Title
- * @method ManyManyList Appointments()
  */
 class AppointmentList extends DataObject
 {
@@ -31,5 +29,6 @@ class AppointmentList extends DataObject
      */
     private static $belongs_many_many = [
         'Appointments' => Appointment::class,
+        'Pages' => SiteTree::class,
     ];
 }
